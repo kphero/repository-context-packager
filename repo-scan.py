33fdc0fbf012b633d7d612120a0151757c0d535b
kphero <k_homen@hotmail.com>
2025-09-06 13:29:31 -0400

    Added paths argument and an analyze function to determine file or directory

--- repo-scan.py
+++ repo-scan.py
@@ -1,6 +1,5 @@
 import argparse
-import datetime
-from pathlib import Path
+import os
 
 parser = argparse.ArgumentParser()
 version_num = "0.1.0"
@@ -12,5 +11,23 @@ parser.add_argument(
     version=f"Repository Context Packager {version_num}"
     )
 
+parser.add_argument(
+    "paths",
+    nargs="*",
+    help="Directory path or files to analyze. Defaults to current directory."
+    )
+
+def analyze_args(arg):
+    absolute_path = os.path.abspath(arg)
+
+    if os.path.isdir(absolute_path):
+        print(f"Analyzing directory: {absolute_path}")
+    elif os.path.isfile(absolute_path):
+        print(f"Analyzing file: {absolute_path}")
+    else:
+        print(f"Invalid path: {absolute_path}")
+
 args = parser.parse_args()
 
+for path in args.paths:
+    analyze_args(path)
